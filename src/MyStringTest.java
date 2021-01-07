ca45a485f98263e45f27a51e86a2bcf5d8bc9b1a
ma851rue <48826778+ma851rue@users.noreply.github.com>
2021-01-07 15:19:38 +0100

    Update MyStringTest.java
    fix

--- src/MyStringTest.java
+++ src/MyStringTest.java
@@ -20,7 +20,7 @@ public class MyStringTest {
     }
 
     @Test
-    public void testEqualsFalseNoInstanceOfMyString() {
+    public void testEqualsFalseNotSameObject() {
         //Setup
         MyString myString = new MyString("a");
 
